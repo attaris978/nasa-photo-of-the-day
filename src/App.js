dba9f725d3d9e8c0b021edcb1c0fad0a7d7d860d
unknown <ryancare@protonmail.com>
2022-03-22 19:29:10 -0500

    Added fetch useEffect to get NASA data, added API Key info, added state for the data and for errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import "./App.css";
-
+const APIKey = "X7hhjc1HgiuvLkLaToKUINEpmr0MrKb9sZeIsDfU";
+const NASAUrl = 'https://api.nasa.gov/planetary/apod?api_key=' + APIKey;
+console.log(NASAUrl);
 function App() {
+  const [data,setData] = useState();
+  const [error,setError] = useState();
+
+  useEffect( () => {
+    fetch(NASAUrl)
+    .then(result => {
+      result.ok ? setData(result.json()) : console.log("Fetch error:" + result.status)
+    })
+    .catch(err => setError(error));
+    console.log(data);
+    },[]);
+
   return (
     <div className="App">
       <p>
